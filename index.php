38d77a4318069ca23512c88dee5349889ae9224e
JohnDoeson <ruperty123@yandex.ru>
2016-08-27 13:58:17 +0300

    первый коммит

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylorotwell@gmail.com>
+ */
+
+
+require __DIR__.'/local/bootstrap/autoload.php';
+$app = require_once __DIR__.'/local/bootstrap/start.php';
+
+
+
+
+
+$app->run();
